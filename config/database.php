142f015a028a87dbe80ea18e3bb3ff370004df88
Pawan Maurya <pawanmauryaji41@gmail.com>
2026-09-25 11:05:49 +0530

    add db connnection

---
+++ config/database.php
@@ -0,0 +1,7 @@
+$host = "localhost";
+$user = "root";
+$password = "";
+$database = "office_os"
+
+ git config --global user.email "pawanmauryaji41@gmail.com"
+  git config --global user.name "Pawan Maurya"
